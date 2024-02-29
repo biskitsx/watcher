88eac2302dc38c41ac94f77a676ce864babce141
Suphakit Songsuwong <kitsugarr@gmail.com>
2024-02-29 23:25:47 +0700

    add track page

---
+++ app/track/page.tsx
@@ -0,0 +1,13 @@
+import Calendar from "@/components/calendar/Calendar";
+import { Container } from "@/components/layout/Container";
+import React from "react";
+
+function Page() {
+  return (
+    <Container>
+      <Calendar />
+    </Container>
+  );
+}
+
+export default Page;
