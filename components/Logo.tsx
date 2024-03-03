696e3bce7c9f9316b99cfabee2397a396ac7bfa0
Suphakit Songsuwong <kitsugarr@gmail.com>
2024-03-03 14:00:03 +0700

    fix logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,8 +3,8 @@ export const Logo = ({}: LogoProps) => {
   return (
     <span>
       <div className="h-1 w-1/2 bg-custom"></div>
-      <h1 className="uppercase text-base font-extrabold tracking-wide">
-        watcher
+      <h1 className=" text-base font-extrabold tracking-wide uppercase">
+        Watcher
       </h1>
     </span>
   );
