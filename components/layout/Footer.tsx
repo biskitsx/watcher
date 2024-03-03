3124158ae4a12c73854cfda114ffeb54af41ecbb
Suphakit Songsuwong <kitsugarr@gmail.com>
2024-03-03 13:58:40 +0700

    change footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -3,7 +3,7 @@ export const Footer = ({}: FooterProps) => {
   return (
     <footer className="footer footer-center p-4 bg-base-300 text-base-content">
       <aside>
-        <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+        <p>Copyright © 2024</p>
       </aside>
     </footer>
   );
