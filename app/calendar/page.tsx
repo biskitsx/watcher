b1d8fd7e10a1ae6001c758af3d54a640f3122d66
Suphakit Songsuwong <kitsugarr@gmail.com>
2024-03-08 15:12:33 +0700

    add breacdcrump

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -3,12 +3,14 @@ import { Container } from "@/components/layout/Container";
 import { getTrendingMovies, getUpcomingMovies } from "@/action/movies";
 
 import React from "react";
+import { BreadcrumbApp } from "@/components/BreadCrumb";
 
 async function Page() {
   const upcomingMovies = await getUpcomingMovies(12);
 
   return (
     <Container>
+      <BreadcrumbApp items={["Home", "Calendars"]} />
       <Calendar items={upcomingMovies} />
     </Container>
   );
---
+++ components/BreadCrumb.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Breadcrumb } from "antd";
+import { title } from "process";
+
+interface BreadcrumbAppProps {
+  items: any[];
+}
+
+export const BreadcrumbApp = ({ items }: BreadcrumbAppProps) => (
+  <Breadcrumb
+    // items={[
+    //   {
+    //     title: "Home",
+    //   },
+    //   {
+    //     title: "Forums",
+    //   },
+    // ]}
+    items={items.map((item) => {
+      return { title: item };
+    })}
+  />
+);
