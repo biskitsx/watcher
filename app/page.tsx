034322601aaabb1466a036426c442a822525201b
Suphakit Songsuwong <kitsugarr@gmail.com>
2024-03-10 12:14:28 +0700

    add anime

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,42 @@
 // "use client";
 
-import Image from "next/image";
-import { getTrendingMovies, getUpcomingMovies } from "@/action/movies";
-import { Suspense } from "react";
-import { Navbar } from "@/components/layout/Navbar";
+import { getTrendingAll, getUpcomingMovies } from "@/action/movies";
 import { MediaSlider } from "@/components/media/MediaSlider";
-import { tmdbImagesURL } from "@/data/baseUrl";
-import { MediaCarousel } from "@/components/media/MediaCarousel";
-import { Container } from "@/components/layout/Container";
 import { HomeCarousel } from "@/components/media/HomeCarousel";
 import { PageContainer } from "@/components/layout/PageContainer";
+import { Container } from "@/components/layout/Container";
+import { getTopAnime } from "@/action/anime";
 
 export default async function Home() {
-  const trendingMovies = await getTrendingMovies(12);
+  const trendingMovies = await getTrendingAll(12);
   const upcomingMovies = await getUpcomingMovies(12);
-
+  const topAnimes = await getTopAnime(12);
   return (
     <PageContainer>
       <HomeCarousel items={trendingMovies} />
-      {/* <MediaCarousel items={upcomingMovies} /> */}
-      <MediaSlider
-        baseUrl={tmdbImagesURL}
-        items={trendingMovies}
-        name="Movies"
-        type="movies"
-        key={0}
-      />
-      <MediaSlider
-        baseUrl={tmdbImagesURL}
-        items={upcomingMovies}
-        type="movies"
-        name="Series"
-        key={1}
-      />
+      <Container>
+        <MediaSlider
+          href="/movies"
+          items={trendingMovies}
+          name="Movies"
+          type="movies"
+          key={0}
+        />
+        <MediaSlider
+          href="/series"
+          items={upcomingMovies}
+          type="series"
+          name="Series"
+          key={1}
+        />
+        <MediaSlider
+          href="/anime"
+          items={topAnimes}
+          type="anime"
+          name="Anime"
+          key={2}
+        />
+      </Container>
     </PageContainer>
   );
 }
