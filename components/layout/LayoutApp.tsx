5a9d4a990ce2d2d8e14658c22de118d5c03ef5c9
Suphakit Songsuwong <kitsugarr@gmail.com>
2025-01-24 14:35:36 +0700

    Add session to Navbar component

--- components/layout/LayoutApp.tsx
+++ components/layout/LayoutApp.tsx
@@ -3,14 +3,17 @@ import { Container } from "./Container";
 import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
 import { FloatButton } from "antd";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOption";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
-export const Layout = ({ children }: LayoutProps) => {
+export const Layout = async ({ children }: LayoutProps) => {
+  const session = await getServerSession(authOptions);
   return (
     <>
-      <Navbar />
+      <Navbar session={session} />
       <Container>{children}</Container>
       <Footer />
     </>
